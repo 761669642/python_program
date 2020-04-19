be361c1800380c9a9ae404df27e52d38f2c75a05
761669642 <761669642@qq.com>
2020-04-19 13:06:57 +0800

    test

---
+++ hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+a = 1
+b = 2.0
+c = 10000000000000000000000000000000000000000
+d = "what's your problem"
+e = "  There is a space  "
+print("Hello world")
+print (a)
+print ("a = %d" % a)
+print (b)
+print ("\tb^3 = %f" % b**3)
+print (c+1)
+print (d + " fuck off")
+print (d.upper())
+print (d.title())
+print (e.rstrip())
+print (e.lstrip())
+print ("wow %f" % (b/2))
---
+++ list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+car = ['benze', 'bwm', 'honda', 'auodi']
+print (car)
+print (car[0].title())
+print (car[-1].upper())
