8dd19209ebfa20d79628d53ce723d21aa7e90706
761669642 <761669642@qq.com>
2020-04-19 19:55:24 +0800

    add many

--- list.py
+++ list.py
@@ -4,3 +4,7 @@ car = ['benze', 'bwm', 'honda', 'auodi']
 print (car)
 print (car[0].title())
 print (car[-1].upper())
+car.append('camery')
+car.insert(1, 'lamboginy')
+print (car[-1].upper()+" " +car[-2].upper())
+print (car)
